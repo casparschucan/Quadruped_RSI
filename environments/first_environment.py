4569d994ab6dd5615be10f13b6afcc8b34f7836d
casparschucan <62501127+casparschucan@users.noreply.github.com>
2020-07-14 23:13:43 +0200

    Update first_environment.py

--- environments/first_environment.py
+++ environments/first_environment.py
@@ -4,21 +4,22 @@ from gym.utils import seeding
 
 
 from stable_baselines.sac.policies import MlpPolicy
-from stable_baselines import SAC
+from stable_baselines import SAC, PPO2
 
 import os
 import math
 import random
 import numpy as np 
 import pybullet as p 
-
-class QuadEnv(gym.Env):
+import pybullet_data
+class Quadruped(gym.Env): 
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
         self.step_counter = 0
         self.episode_counter = 0
-        p.connect(p.DIRECT)
+        p.connect(p.GUI)
+
         self.action_space = spaces.Box(
             np.array([-0.55, -0.3, -0.55, -0.3, -1.05, -0.8, -1.05, -0.8]),
             np.array([1.05, 0.8, 1.05, 0.8, 0.55, 0.3, 0.55, 0.3])
@@ -33,10 +34,10 @@ class QuadEnv(gym.Env):
         new_position, new_orientationq = p.getBasePositionAndOrientation(self.robotId)
         new_orientation = p.getEulerFromQuaternion(new_orientationq)
         done = False
-        if abs(new_orientation[0]) > 0.5 or abs(new_orientation[1]) > 0.5:
+        if abs(new_orientation[0]) > 1 or abs(new_orientation[1]) > 0.5:
             done = True
-        reward = (((new_position[0] - position[0]) ** 2 + (new_position[1] - position[1]) ** 2)
-                  - 0.005 * math.exp(abs(new_orientation[0]) + abs(new_orientation[1])))
+        reward = (1000 * ((position[0] - new_position[0]))
+                  - (abs(new_orientation[0]) + abs(new_orientation[1])))
         joint_info = p.getJointStates(self.robotId, [0, 1, 3, 4, 6, 7, 9, 10])
         motor_positions = []
         for motor_info in joint_info:
@@ -44,13 +45,9 @@ class QuadEnv(gym.Env):
         base_ang_velocity = p.getBaseVelocity(self.robotId)[1]
         base_angles_and_velocity = [new_orientation[0], new_orientation[1],
                                     base_ang_velocity[0], base_ang_velocity[1]]
-        self.observation = base_angles_and_velocity + motor_positions
-        info = {
-            "episode" : self.episode_counter,
-            "pos" : new_position
-        }
+        self.observation = np.array(base_angles_and_velocity + motor_positions)
         self.step_counter += 1
-        return self.observation, reward, done, info
+        return self.observation, reward, done, {}
 
 
     def reset(self):
@@ -58,10 +55,13 @@ class QuadEnv(gym.Env):
         self.step_counter = 0
         p.resetSimulation()
         p.setGravity(0, 0, -9.81)
-        p.resetDebugVisualizerCamera(1, 45, -20, [0, 0, 0])
-        planeId = p.loadURDF("plane_col.urdf")
-        self.robotId = p.loadURDF("creation.urdf", [0, 0, 0.28])
-        self.observation = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        p.resetDebugVisualizerCamera(5, 45, -20, [0, 0, 0])
+        self.visual_shape = p.createVisualShape(p.GEOM_BOX, halfExtents = [10,10,0.1])
+        self.coll_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents = [10,10,0.1])
+        print(self.episode_counter)
+        self.planeId = p.loadURDF("/Users/caspa/VSCode/RSI_code/experiments/robots/terrain.urdf", [0, 0, 0])
+        self.robotId = p.loadURDF("/Users/caspa/VSCode/RSI_code/experiments/robots/version1.urdf", [0, 0, 0.3])
+        self.observation = np.array([0]*12)
         return self.observation
 
     def render(self, mode='human'):
@@ -89,9 +89,3 @@ class QuadEnv(gym.Env):
 
     def close(self):
         p.disconnect()
-
-env = QuadEnv()
-
-model = SAC(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=200, log_interval=10)
-model.save("first_test")
